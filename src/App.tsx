acc1e1e2227a6554b3d92dba73630e1d75dfca82
Ed Tosoy <edberto.tosoy@gmail.com>
2020-12-04 01:48:23 +0800

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React, { ReactElement } from "react";
+import React from "react";
+import { MenuProvider } from "./ShopContext/menuContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import ShoppingCart from "./Pages/ShoppingCart/ShoppingCart";
+import Auth from "./Pages/Auth/Auth";
+import Register from "./Pages/Auth/Register/Register";
+import Shopping from "./Pages/Shopping/Shopping";
+import Header from "./Component/Header/Header";
+import CopyRight from "./Component/CopyRight/CopyRight";
+import Footer from "./Component/Footer/Footer";
 
-interface Props {}
-
-export default function App({}: Props): ReactElement {
-  return <div>hello</div>;
+function App() {
+  return (
+    <MenuProvider>
+      <div className="app">
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cart" component={ShoppingCart} />
+            <Route exact path="/auth" component={Auth} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/shopping/:category" component={Shopping} />
+          </Switch>
+          <Footer />
+          <CopyRight />
+        </Router>
+      </div>
+    </MenuProvider>
+  );
 }
+
+export default App;
